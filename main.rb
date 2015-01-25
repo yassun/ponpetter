af60558ddc9fc547dc51270dfdabb95a7a4b7f95
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-25 11:28:29 +0900

    add main.rb

---
+++ main.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'logger'
+require 'unicorn'
+require 'haml'
+require 'sass'
+
+class MainApp < Sinatra::Base
+
+    # css
+    get %r{^/(.*)\.css$} do
+        scss :"style/#{params[:captures].first}"
+    end
+
+    # root
+    get '/' do
+        haml :index
+    end
+
+end
+
