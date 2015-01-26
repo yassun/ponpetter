263413243f7ea03ade5b68044e48a41acbb4ca45
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-26 09:39:16 +0900

    add TweetSearch#client

--- app/tweet_search.rb
+++ app/tweet_search.rb
@@ -2,5 +2,19 @@ require 'twitter'
 
 module Ponpetter
   class TweetSearch
+    def initialize
+      @client = client
+    end
+
+    private
+    def client
+      Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV["TW_CONSUMER_KEY"]
+        config.consumer_secret = ENV["TW_CONSUMER_SECRET"]
+        config.access_token = ENV["TW_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TW_ACCESS_SECRET"]
+      end
+    end
+
   end
 end
