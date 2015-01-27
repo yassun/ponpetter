56e57d8f222459ebdf7439effc957462efeeb54b
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-27 11:28:32 +0900

    add rspec

---
+++ spec/app_spec.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "App" do
+  include Rack::Test::Methods
+  def app
+    @app ||= MainApp
+  end
+
+  describe "GET /" do
+    before { get '/' }
+    it "ステータスコード200が返ること" do
+      expect(last_response.status).to be 200
+    end
+  end
+end
