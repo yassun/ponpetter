26e9d55c5764842cc8cf5e5399bf6cb081038def
Taga Yasunori <yasukun.taga@gmail.com>
2015-02-02 11:08:47 +0900

    add app_spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,9 +7,30 @@ describe "App" do
   end
 
   describe "GET /" do
-    before { get '/' }
-    it "ステータスコード200が返ること" do
-      expect(last_response.status).to be 200
+    before :each do
+      mr = MockRedis.new
+      mr.set(Date.today.to_s, 100)
+      tweets = [
+        {
+          id: 1234567890,
+          img: "http://pbs.twimg.com/profile_images/img/xxxxx.jpeg",
+          autor: "hoge",
+          text: "お腹痛い",
+          time: "2015-02-01 16:57:17 +0900"
+        }
+      ]
+      mr.set('tweets', Marshal.dump(tweets))
+
+      allow(Ponpetter::Redis).to receive(:connect).and_return(mr)
+    end
+
+    subject do
+      get '/'
+      last_response
+    end
+
+    it 'ステータスコード200を返すこと' do
+      expect(subject.status).to be 200
     end
   end
 end
