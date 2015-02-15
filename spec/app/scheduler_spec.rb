06eb5675ad91a73f72b5bdf056ff84711abcfdda
Taga Yasunori <yasukun.taga@gmail.com>
2015-02-15 09:50:35 +0900

    グラフデータをセット型からリスト型に変更 テスト修正

--- spec/app/scheduler_spec.rb
+++ spec/app/scheduler_spec.rb
@@ -27,46 +27,65 @@ describe "Scheduler" do
   end
 
   describe "#execute" do
-    before do
-      Ponpetter::Scheduler.execute
-    end
 
-    it "前回処理日付に今日の日付が設定されていること" do
-      expect(mr.get('last-date')).to eq "2015-02-01"
-    end
-
-    it "since_idが更新されていること" do
-      expect(mr.get('since-id')).to eq "1234567890"
-    end
-
-    it "tweetが保存されていること" do
-      expect(Marshal.load(mr.get('tweets'))).to eq tweets
-    end
-
-    it "ポンペ数が更新されていること" do
-      expect(mr.get('ponpe-cnt')).to eq "1"
-    end
-
-    context "日替わり発生時" do
+    context "日替わり未発生" do
       before do
-        mr.set('last-date','2014-01-31')
-        mr.set('ponpe-cnt','100')
         Ponpetter::Scheduler.execute
       end
 
-      it "グラフのラベルが追加されていること" do
-        expect(mr.zrange('graph-labels', 0, 30)).to match_array ["2014-01-31"]
+      it "前回処理日付に今日の日付が設定されていること" do
+        expect(mr.get('last-date')).to eq "2015-02-01"
       end
 
-      it "グラフの値が追加されていること" do
-        expect(mr.zrange('graph-values', 0, 30)).to match_array ["100"]
+      it "since_idが更新されていること" do
+        expect(mr.get('since-id')).to eq "1234567890"
       end
 
-      it "ポンペ数が初期化されていること" do
+      it "tweetが保存されていること" do
+        expect(Marshal.load(mr.get('tweets'))).to eq tweets
+      end
+
+      it "ポンペ数が更新されていること" do
         expect(mr.get('ponpe-cnt')).to eq "1"
       end
-
     end
 
+    context "日替わり発生時" do
+
+      context "グラフデータ格納最大数を超えていない場合" do
+        before do
+          mr.set('last-date','2014-01-31')
+          mr.set('ponpe-cnt','100')
+          Ponpetter::Scheduler.execute
+        end
+
+        it "グラフのラベルが追加されていること" do
+          expect(mr.lrange('graph-labels', 0, -1)).to match_array ["2014-01-31"]
+        end
+
+        it "グラフの値が追加されていること" do
+          expect(mr.lrange('graph-values', 0, -1)).to match_array ["100"]
+        end
+
+        it "ポンペ数が初期化されていること" do
+          expect(mr.get('ponpe-cnt')).to eq "1"
+        end
+      end
+
+      context "グラフデータ格納最大数を超えている場合" do
+        it "一番古いグラフデータが削除されていること" do
+          mr.set('last-date','2014-01-31')
+          mr.set('ponpe-cnt','100')
+          365.times do | cnt | 
+            mr.rpush('graph-labels', cnt)
+            mr.rpush('graph-values', cnt)
+          end
+
+          Ponpetter::Scheduler.execute
+          expect(mr.lpop('graph-labels')).to eq '1'
+          expect(mr.lpop('graph-values')).to eq '1'
+        end
+      end
+    end
   end
 end
