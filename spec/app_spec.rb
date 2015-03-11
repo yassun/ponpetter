32242b1f73937b7230f790b38672ad85da78df95
Taga Yasunori <yasukun.taga@gmail.com>
2015-03-11 11:53:37 +0900

    テストをReact.jsに対応

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,66 +1,114 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "App" do
-  include Rack::Test::Methods
-  def app
-    @app ||= Sinatra::Application
-  end
 
-  describe "GET /" do
+  let(:set_up_mock_redis) {
+    allow(Date).to receive(:today).and_return(Date.new(2015, 2, 1))
+    mr = MockRedis.new
+    mr.set('ponpe-cnt', 100)
+    tweets = [
+      {
+        id: 1234567890,
+        img: "http://pbs.twimg.com/profile_images/img/xxxxx.jpeg",
+        autor: "hoge",
+        text: "お腹痛い",
+        time: "2015-02-01 16:57:17 +0900"
+      }
+    ]
+    mr.set('tweets', Marshal.dump(tweets))
+    mr.rpush('graph-labels', "2015-01-31")
+    mr.rpush('graph-values', 200)
+    allow(Ponpetter::Redis).to receive(:connect).and_return(mr)
+  }
+
+  describe "GET /", js: true do
+    include Capybara::DSL
+
+    before :all do
+      Capybara.app = Sinatra::Application.new
+    end
+
     before :each do
-      allow(Date).to receive(:today).and_return(Date.new(2015, 2, 1))
-
-      mr = MockRedis.new
-      mr.set('ponpe-cnt', 100)
-      tweets = [
-        {
-          id: 1234567890,
-          img: "http://pbs.twimg.com/profile_images/img/xxxxx.jpeg",
-          autor: "hoge",
-          text: "お腹痛い",
-          time: "2015-02-01 16:57:17 +0900"
-        }
-      ]
-      mr.set('tweets', Marshal.dump(tweets))
-
-      mr.rpush('graph-labels', "2015-01-31")
-      mr.rpush('graph-values', 200)
-
-      allow(Ponpetter::Redis).to receive(:connect).and_return(mr)
+      set_up_mock_redis
+      visit '/'
     end
 
-    subject do
-      get '/'
-      last_response
-    end
+    subject { page }
 
-    it 'ステータスコード200を返すこと' do
-      expect(subject.status).to be 200
+    it { should have_title('Ponpetter') }
+
+    it 'ステータスコードが200を返すこと' do
+      expect(page.status_code).to eq(200)
     end
 
     it 'ツイート合計数が表示されていること' do
-      expect(subject.body).to include '<h2>本日のポンペ数 100人</h2>'
-    end
-
-    it 'グラフ用の値が設定されていること' do
-      expect(subject.body).to include 'labels : ["2015-01-31"]'
-      expect(subject.body).to include 'data : ["200"]'
+      within("div.counter") do
+        should have_content('本日のポンペ数 100人')
+      end
     end
 
     it 'ユーザーの画像が表示されていること' do
-      expect(subject.body).to include '<img src="http://pbs.twimg.com/profile_images/img/xxxxx.jpeg">'
+      within("div.tweets.row") do
+        should have_selector("img[src$='http://pbs.twimg.com/profile_images/img/xxxxx.jpeg']")
+      end
     end
 
     it 'ユーザー名が表示されていること' do
-      expect(subject.body).to include '<a href="https://twitter.com/hoge" target="_blank">@hoge</a>'
+      within("div.tweets.row") do
+        should have_link('@hoge', :href => 'https://twitter.com/hoge')
+      end
     end
 
     it 'テキストが表示されていること' do
-      expect(subject.body).to include 'お腹痛い'
+      within("div.tweets.row") do
+        should have_content('お腹痛い')
+      end
     end
 
     it '時刻が表示されていること' do
-      expect(subject.body).to include '<a href="https://twitter.com/hoge/status/1234567890" target="_blank">2015-02-01 16:57:17 +0900</a>'
+      within("div.tweets.row") do
+        should have_link('2015-02-01 16:57:17 +0900', :href => 'https://twitter.com/hoge/status/1234567890')
+      end
+    end
+
+  end
+
+  describe 'GET /ponpe.json' do
+    include Rack::Test::Methods
+    def app
+      @app ||= Sinatra::Application
+    end
+
+    before :each do
+      set_up_mock_redis
+    end
+
+    subject do
+      get '/ponpe.json'
+      last_response
+    end
+
+    let(:json) {
+      JSON.parse(subject.body)
+    }
+
+    it 'ステータスコードが200を返すこと' do
+      expect(subject.status).to be 200
+    end
+
+    it 'ツイート合計数が返されていること' do
+      expect(json['ponpe_cnt']).to eq "100"
+    end
+
+    it 'ツイートデータが返されていること' do
+      expect(json['tweets']).to match([{
+            "id"=>1234567890, "img"=>"http://pbs.twimg.com/profile_images/img/xxxxx.jpeg",
+            "autor"=>"hoge", "text"=>"お腹痛い", "time"=>"2015-02-01 16:57:17 +0900"
+          }])
+    end
+
+    it 'グラフデータが返されていること' do
+      expect(json['graph']).to match({"labels"=>["2015-01-31"], "values"=>["200"]})
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,27 @@ require 'rubygems'
 require 'sinatra'
 require 'rack/test'
 require 'rspec'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
 require 'mock_redis'
 
 set :environment, :test
 set :run, false
 set :raise_errors, true
 set :logging, false
+
+Capybara.default_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {
+                    js_errors: false,
+                    timeout: 1000,
+                    phantomjs_options: [
+                              '--load-images=no',
+                              '--ignore-ssl-errors=yes',
+                              '--ssl-protocol=any'],
+                    inspector: true,
+                    })
+end
+
 
